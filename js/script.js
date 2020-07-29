52da770e35142d39a95b72953e06953189a53b5b
Mathias <mathias.grandin@gmail.com>
2020-07-29 15:53:36 +0200

    Modifiche al css. unica funzione per messaggio e risposta casuale. Aggiunta condizione di invio solamente a messaggio non vuoto

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ var messaggiRandom = ['Ok', 'Ciao', 'La penso proprio come te', 'Sei grande'];
 // Click
 $('#send').click(function() {
   aggiungi();
-  randomReply();
 });
 
 // Invio tastiera
@@ -12,40 +11,37 @@ $('#message').keydown(keyboard);
 
 // Funzioni
 
-// messaggio utente
+// messaggio utente e risposta casuale
 function aggiungi(){
     var messaggio = $('#message').val();
-    var chat = $('.template .chat-text-ctr').clone();
-    chat.find('.chat-text').addClass('green');
-    chat.find('.chat-text p').append(messaggio);
-    $('.main-chat').append(chat);
-    var d = new Date();
-    var minutes = ("0" + d.getMinutes()).substr(-2);
-    var time = d.getHours() + ':' + minutes;
-    chat.find('.chat-time small').append(time);
-    $('#message').val('');
-}
-
-// Funzione per risposta casuale
-function randomReply() {
-    setTimeout(function(){
-      var rispostaRandom = messaggiRandom[random(0, (messaggiRandom.length - 1))]
-      var answer = $('.template .chat-text-ctr').clone();
-      answer.find('.chat-text').addClass('white');
-      answer.find('.chat-text p').append(rispostaRandom);
-      $('.main-chat').append(answer);
+    // Condizione per messaggio non vuoto
+    if (messaggio != '') {
+      var chat = $('.template .chat-text-ctr').clone();
+      chat.find('.chat-text').addClass('green');
+      chat.find('.chat-text p').append(messaggio);
+      $('.main-chat').append(chat);
+      // ora attuale
       var d = new Date();
       var minutes = ("0" + d.getMinutes()).substr(-2);
       var time = d.getHours() + ':' + minutes;
-      answer.find('.chat-time small').append(time);
-    }, 1000);
+      chat.find('.chat-time small').append(time);
+      $('#message').val('');
+      // Risposta casuale dopo un secondo
+      setTimeout(function(){
+        var rispostaRandom = messaggiRandom[random(0, (messaggiRandom.length - 1))]
+        var answer = $('.template .chat-text-ctr').clone();
+        answer.find('.chat-text').addClass('white');
+        answer.find('.chat-text p').append(rispostaRandom);
+        $('.main-chat').append(answer);
+        answer.find('.chat-time small').append(time);
+      }, 1000);
+  }
 }
 
 //  Funzione per la tastiera
 function keyboard() {
   if (event.which == 13 || event.keyCode == 13) {
     aggiungi();
-    randomReply();
   }
 }
 
